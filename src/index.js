4be83e9eecc45a90ecc128926d40fb0e689588bd
alexanderrabetsky <3820168Rabetsky>
2019-03-24 22:25:24 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let baseMultipliers = getMultipliers(base);
+    let countMinZeros = [];
+    function getMultipliers (numb) {
+        let objOfNumber = {};
+        for (let i = 2; numb !== 1; i++) {
+            if (numb%i === 0) {
+                if ( i in objOfNumber) {
+                    ++objOfNumber[i]['pow'];
+                }
+                else {
+                    objOfNumber[i] = {
+                        'pow': 1
+                    }
+                }
+                numb = numb/i;
+                i--;
+            }
+        }
+        return objOfNumber;
+    }
+
+    for (let i in baseMultipliers) {
+        let countNumb = 0;
+
+        for (let j = 1; Math.floor(number / Math.pow(i, j)) > 0; j++) {
+            countNumb += Math.floor(number / Math.pow(i, j));
+        }
+        countMinZeros.push(Math.floor(countNumb/baseMultipliers[i]['pow']));
+    }
+    return Math.min.apply(null,countMinZeros);
 }
